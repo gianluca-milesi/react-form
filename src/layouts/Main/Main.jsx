d6a95fe0f98f7af45fcd0a1bc67d146676873e5c
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-12-02 17:15:04 +0100

    ADD: form onSubmit function

--- src/layouts/Main/Main.jsx
+++ src/layouts/Main/Main.jsx
@@ -1,18 +1,42 @@
 import style from "./Main.module.css"
 import PostCard from "../../components/PostCard/PostCard.jsx"
 import posts from "../../data/posts.js"
+import initialPosts from "../../data/posts.js"
 import Tags from "../../components/Tags/Tags.jsx"
+import { useState } from "react"
 
 function Main() {
 
     const publishedPosts = posts.filter((post) => post.published === true)
 
 
+    const [allPosts, setAllPosts] = useState(initialPosts)
+    const [title, setTitle] = useState("Pinguino")
+
+    function onSubmit(event) {
+        event.preventDefault()
+
+        const newTitle = title.trim()
+
+        const newPost = {
+            title: newTitle,
+            image: undefined,
+            content: "content",
+            tags: [],
+            published: true
+        }
+
+        setAllPosts([...publishedPosts, newPost])
+        // console.log(newPost)
+        console.log(allPosts)
+    }
+
+
     const tags = []
     posts.forEach(post => {
 
         const postTags = post.tags
-        console.log(postTags)
+        // console.log(postTags)
 
         postTags.forEach((tag) => {
             if (!tags.includes(tag)) {
@@ -28,9 +52,9 @@ function Main() {
                 <section className={style.form_section}>
                     <div className="container">
                         <p>Inserisci un nuovo post</p>
-                        <form action="">
-                            <input type="text" placeholder="Titolo del post"/>
-                            <input type="submit" />
+                        <form onSubmit={onSubmit} action="">
+                            <input type="text" placeholder="Titolo del post" onChange={(event) => setTitle(event.target.value)} value={title} />
+                            <input type="submit" value="Aggiungi" />
                         </form>
                     </div>
                 </section>
